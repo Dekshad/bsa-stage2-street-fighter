913a997c7a5a62eff57e895eaa99b608d40052bc
Dekshad <camelbarley@gmail.com>
2026-05-29 03:20:55 +0300

    + arena component and winner modal display

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,21 @@
 import showModal from './modal';
+import { createFighterImage } from '../fighterPreview';
+import createElement from '../../helpers/domHelper';
 
 export default function showWinnerModal(fighter) {
-    // call showModal function
+    const bodyElement = createElement({
+        tagName: 'div',
+        className: 'modal-body'
+    });
+
+    const imgElement = createFighterImage(fighter);
+    bodyElement.append(imgElement);
+
+    showModal({
+        title: `${fighter.name} is the Winner!`,
+        bodyElement,
+        onClose: () => {
+            window.location.reload();
+        }
+    });
 }
